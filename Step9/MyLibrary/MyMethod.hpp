3fb512f6e1a63bd4b92f19a0a1a7311cb7642b77
Geroshabu <oonstks@gmail.com>
2022-08-13 19:59:45 +0900

    Step9: MyMethods ライブラリを Shared ライブラリとする

--- Step9/MyLibrary/MyMethod.hpp
+++ Step9/MyLibrary/MyMethod.hpp
@@ -1,3 +1,13 @@
+#if defined(_WIN32)
+#  if defined(EXPORTING_PRINT)
+#    define DECLSPEC __declspec(dllexport)
+#  else
+#    define DECLSPEC __declspec(dllimport)
+#  endif
+#else // non windows
+#  define DECLSPEC
+#endif
+
 namespace mymodule {
-    void print();
+    void DECLSPEC print();
 }
